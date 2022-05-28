63643f695551100af382e2bef9ac0f8e0b722fd2
sSsaAa3 <103163095+sSsaAa3@users.noreply.github.com>
2022-05-27 21:10:18 -0500

    Add files via upload

---
+++ funcs.js
@@ -0,0 +1,14 @@
+function cTodo1() {
+     document.getElementById("titulo").innerHTML = "¿QUE ES LA BASURA ELECTRÓNICA?";  
+     document.getElementById("pix").src = "ec.png";               
+     document.getElementById("infr").innerHTML = "La basura electronica,aporta gran parte de la contaminación  y del cambio climático en el planeta.Naciones Unidas calcula que para 2040. las emisiones de carbono provenientes del uso de aparatos electrónicos representarán el 14% de las emisiones totales. La primera gran acción es evitar la generación de residuos electrónicos a partir de hacer un consumo responsable, es importante cuestionarse si realmente los necesitas o si todavía son funcionales. Asimismo, antes de comprar algo nuevo establece criterios basados en calidad, tiempo de vida, uso y materiales de cada producto esto para no generar mas. Si tus aparatos presentan alguna falla por el uso o requieren alguna actualización, llévalo a reparar directamente con el fabricante. Otra alternativa es reciclar los aparatos electrónicos y algunas de sus partes y componentes.";  
+}
+
+
+function cTodo2() {
+     document.getElementById("titulo").innerHTML = "ECOTEC";
+     document.getElementById("pix").src = "ecos.png";
+     document.getElementById("infr").innerHTML = "TE INVITAMOS A QUE PARTICIPES EN LA ACTIVIDAD Del día 10 de mayo a 3 DE JUNIO de junio se estará juntando basura electrónica en el plantel (CETIS62) ubicado en AV LEONA VICARIO SN, EL DURAZNO, 36790 SALAMANCA GTO, el propósito de esta actividad es promover el reciclaje de este tipo de desechos en las principales medidas para reducir la contaminación electrónica, Para evitar la generación de esta basura pon en practica estos hábitos: 1_Haz un consumo responsable 2_dale una segunda vida arreglalo o regalalo 3_elige aparatos duraderos";
+}
+
+     
---
+++ rfuncs.js
@@ -0,0 +1,29 @@
+function cTodo1() {
+     document.getElementById("titulo").innerHTML = "¿QUE ES LA BASURA ELECTRÓNICA?";  
+     document.getElementById("pix").src = "";               
+     document.getElementById("infr").innerHTML = "son desechos tecnológicos, desechos electrónicos o basura tecnológica conocida por el concepto RAEE 
+(residuos de aparatos eléctricos y electrónicos ). El tratamiento inadecuado de los RAEE.
+ Puede ocasionar graves impactos al medio ambiente y poner en riesgo la salud humana.
+
+TE INVITAMOS A QUE PARTICIPES EN LA ACTIVIDAD
+Del día 10 de mayo a 3 DE JUNIO de junio se estará juntando basura electrónica en el plantel (CETIS62) ubicado en AV LEONA VICARIO SN, EL DURAZNO, 36790 SALAMANCA GTO
+el propósito de esta actividad es promover el reciclaje de este tipo de desechos en las principales medidas para reducir la contaminación electrónica, por ejemplo un smartphone se puede reciclar hasta un %90 pues sus componentes pueden ser útiles para otros dispositivos
+Para evitar la generación de esta basura pon en practica estos hábitos: 1_Haz un consumo responsable 2_dale una segunda vida arreglalo o regalalo 3_elige aparatos duraderos";  
+}
+
+
+function cTodo2() {
+     document.getElementById("titulo").innerHTML = "SIMI";
+     document.getElementById("pix").src = "car.jpg";
+     document.getElementById("infr").innerHTML = "cada vez más abundantes en el mundo, son responsables en gran parte de la contaminación del suelo, agua y aire y del cambio climático en el planeta.
+Naciones Unidas calcula que para 2040
+las emisiones de carbono provenientes de la producción y el uso de aparatos electrónicos representarán el 14% de las emisiones totales.
+La primera gran acción es evitar la generación de residuos electrónicos a partir de hacer un consumo responsable y extender el tiempo de vida de los aparatos electrónicos
+es importante cuestionarse si realmente lo necesitas y si tu celular o computadora todavía son funcionales, esto para evitar dejarnos llevar por la moda
+Asimismo, antes de comprar algo nuevo establece criterios basados en calidad, tiempo de vida, uso y materiales de cada producto; no te guíes por la publicidad, marca, moda o novedad.
+Si tus aparatos presentan alguna falla por el uso o requieren alguna actualización, 
+llévalo a reparar directamente con el fabricante o apoya la economía local yendo a algún sitio de reparación en tu comunidad.
+Otra alternativa es reciclar los aparatos electrónicos y algunas de sus partes y componentes. De las más de 1.1 millones toneladas de basura electrónica que se desechan al año en México, menos del 10% se logra reciclar";
+}
+
+     
